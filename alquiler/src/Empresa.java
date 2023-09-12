7a42714011f9b1545610fb826de1901ea7f62c0b
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-12 17:25:19 -0500

    Add files via upload

---
+++ alquiler/src/Empresa.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Empresa
+{
+    private ArrayList<Vehiculo> vehiculos;
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Factura> facturas;
+
+    public Empresa()
+    {
+        vehiculos = new ArrayList<>();
+        clientes = new ArrayList<>();
+        facturas = new ArrayList<>();
+    }
+
+    // Método para cargar vehículos en la empresa
+    public void cargarVehiculo(Vehiculo vehiculo)
+    {
+        vehiculos.add(vehiculo);
+    }
+
+    // Método para alquilar un vehículo a un cliente
+    public void alquilarVehiculo(Cliente cliente, Vehiculo vehiculo, Date fechaInicial, Date fechaFinal)
+    {
+        // Verificar si el vehículo está disponible
+        if (vehiculo.getEstado().equals("disponible"))
+        {
+            vehiculo.setEstado("ocupado");
+
+            // Calcular el costo del alquiler (puedes implementarlo según tus reglas)
+            double costoAlquiler = calcularCostoAlquiler(vehiculo, fechaInicial, fechaFinal);
+
+
+            // Crear una factura para el alquiler
+            Factura factura = new Factura(cliente, vehiculo, costoAlquiler, fechaInicial, fechaFinal);
+
+            // Agregar la factura a la lista de facturas
+            facturas.add(factura);
+        }
+        else
+        {
+            System.out.println("El vehículo no está disponible para alquiler.");
+        }
+    }
+
+    // Método para mostrar la lista de vehículos de la empresa
+    public void mostrarVehiculos()
+    {
+        for (Vehiculo vehiculo : vehiculos)
+        {
+            System.out.println(vehiculo);
+        }
+    }
+
+    // Método para consultar el estado de un vehículo (libre u ocupado)
+    public String consultarEstadoVehiculo(Vehiculo vehiculo)
+    {
+        return vehiculo.getEstado();
+    }
+
+    // Método para calcular el costo de alquiler (debes implementarlo según tus reglas)
+    private double calcularCostoAlquiler(Vehiculo vehiculo, Date fechaInicial, Date fechaFinal)
+    {
+        // Implementa aquí la lógica para calcular el costo de alquiler según el tipo de vehículo, fechas, etc.
+        // Puedes acceder a los atributos del vehículo para calcular el costo.
+        // El ejemplo aquí es simplemente un placeholder.
+        return 0.0;
+    }
+}
