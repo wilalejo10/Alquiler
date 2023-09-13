fd8ee4c376290dcf4616833c66aeb55da0ee6d39
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-13 18:23:33 -0500

    Update Vehiculo.java

--- alquiler/src/Vehiculo.java
+++ alquiler/src/Vehiculo.java
@@ -1,78 +1,70 @@
+import javax.swing.*;
 
-    public class Vehiculo
+public class Vehiculo {
+    private String Placa;
+    private int Tipo; //carga o pasajero
+    private String Capacidad;
+    private String Estado;
+    public int precio;
+
+    public String getPlaca()
     {
-        private String placa;
-        private String tipo; // Puede ser "carga" o "pasajeros"
-        private double valorPorDia;
-        private String estado; // Puede ser "ocupado" o "disponible"
-        private int capacidad;
-
-        public Vehiculo(String placa, String tipo, double valorPorDia, String estado, int capacidad)
-        {
-            this.placa = placa;
-            this.tipo = tipo;
-            this.valorPorDia = valorPorDia;
-            this.estado = estado;
-            this.capacidad = capacidad;
-        }
-
-        public String getPlaca()
-        {
-            return placa;
-        }
-
-        public void setPlaca(String placa)
-        {
-            this.placa = placa;
-        }
-
-        public String getTipo()
-        {
-            return tipo;
-        }
-
-        public void setTipo(String tipo)
-        {
-            this.tipo = tipo;
-        }
-
-        public double getValorPorDia()
-        {
-            return valorPorDia;
-        }
-
-        public void setValorPorDia(double valorPorDia)
-        {
-            this.valorPorDia = valorPorDia;
-        }
-
-        public String getEstado()
-        {
-            return estado;
-        }
-
-        public void setEstado(String estado)
-        {
-            this.estado = estado;
-        }
-
-        public int getCapacidad()
-        {
-            return capacidad;
-        }
-
-        public void setCapacidad(int capacidad)
-        {
-            this.capacidad = capacidad;
-        }
-        public String toString()
-        {
-            return "Vehiculo{" +
-                    "placa='" + placa + '\'' +
-                    ", tipo='" + tipo + '\'' +
-                    ", valorPorDia=" + valorPorDia +
-                    ", estado='" + estado + '\'' +
-                    ", capacidad=" + capacidad +
-                    '}';
-        }
+        return Placa;
     }
+    public void setPlaca(String placa)
+    {
+        Placa = placa;
+    }
+    public int getTipo()
+    {
+        return Tipo;
+    }
+    public void setTipo(int tipo)
+    {
+        Tipo = tipo;
+    }
+    public String getCapacidad()
+    {
+        return Capacidad;
+    }
+    public void setCapacidad(String capacidad)
+    {
+        Capacidad = capacidad;
+    }
+    public String getEstado()
+    {
+        return Estado;
+    }
+
+    public void setEstado(String estado)
+    {
+        Estado = estado;
+    }
+
+    public int calculoVehiculo()
+    {
+
+        int tipo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese " + "1" + " si el vehiculo es de carga o " + "2" + " si el vehiculo es de pasajeros"));
+        setTipo(tipo);
+        if (getTipo() == 1) {
+            String capacidad = JOptionPane.showInputDialog("De que capacidad desea el vehiculo de carga?\n" + "-Pequeña(hasta 3.0 toneladas)\n" + "-Mediana(4.0 a 5.0 toneladas=\n" + "-Pesada(6.0 a 10 toneladas) :");
+            setCapacidad(capacidad);
+            switch (getCapacidad()){
+                case "Pequeña": precio = 200000;break;
+                case "Mediana": precio = 450000;break;
+                case "Pesada": precio = 800000;break;
+                default: JOptionPane.showMessageDialog(null,"El nombre del vehiculo ingresado es erroneo");
+            }
+        } else if (getTipo()==2) {
+            String capacidad = JOptionPane.showInputDialog("Que vehiculo desea alquilar?\n: "+"-Pequeño(10 personas maximo)\n"+"-Mediano(20 personas maximo)\n"+"-Grande(30 personas maximo)");
+            setCapacidad(capacidad);
+            switch (getCapacidad()){
+                case "Pequeño": precio = 80000;break;
+                case "Mediano": precio = 130000;break;
+                case "Grande": precio = 170000;break;
+                default:JOptionPane.showMessageDialog(null,"El nombre del vehiculo ingresado es erroneo");
+            }
+        }
+        return precio;
+    }
+}
