1adc7bdcd300a0a4abc747f7b16acff0f115ba6e
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-13 21:14:09 -0500

    Update Factura.java

--- src/Factura.java
+++ src/Factura.java
@@ -3,39 +3,26 @@ import java.util.Date;
 public class Factura
 {
     public Cliente cliente;
-    public int id;
-    public int telefono;
     public Vehiculo vehiculo;
     public Date fechaInicial;
     public Date fechaFinal;
 
-    public Factura(Cliente cliente, int id, int telefono, Vehiculo vehiculo, Date fechaInicial, Date fechaFinal)
-    {
+    public Factura(Cliente cliente, Vehiculo vehiculo, Date fechaInicial, Date fechaFinal) {
         this.cliente = cliente;
-        this.id = id;
-        this.telefono= telefono;
         this.vehiculo = vehiculo;
         this.fechaInicial = fechaInicial;
         this.fechaFinal = fechaFinal;
     }
 
-    public Factura(Cliente cliente, Vehiculo vehiculo, Date fechaInicial, Date fechaFinal) { }
-
-    public double calcularCostoAlquiler()
-    {
-        long diferenciaDias = (fechaFinal.getTime() - fechaInicial.getTime()) / (24 * 60 * 60 * 1000);
-
-        if (vehiculo.getTipo().equalsIgnoreCase("pasajeros"))
-        {
+    public double calcularCostoAlquiler() {
+        long diferenciaDias = ((fechaFinal.getTime() - fechaInicial.getTime()) / (24 * 60 * 60 * 1000)) +1; //reta de las fechas sobre los valores de mili, seg, min, hor, y el +1 para no cobrar 0 en caso de tener la misma fecha
+        if (vehiculo.getTipo().equalsIgnoreCase("pasajeros")) {
             return vehiculo.getCostoPorDia() * diferenciaDias;
-        } else if (vehiculo.getTipo().equalsIgnoreCase("carga"))
-        {
+        } else if (vehiculo.getTipo().equalsIgnoreCase("carga")) {
             double toneladasCarga = vehiculo.getCapacidad();
             return toneladasCarga * vehiculo.getCostoPorDia() * diferenciaDias;
-        } else
-        {
+        } else {
             return 0.0;
         }
     }
 }
-
