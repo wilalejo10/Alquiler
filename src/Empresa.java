161a917987a9015aced8538de0f13bdab786b128
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-13 21:13:25 -0500

    Update Empresa.java

--- src/Empresa.java
+++ src/Empresa.java
@@ -3,32 +3,30 @@ import java.util.ArrayList;
 import java.util.Date;
 public class Empresa
 {
-    private final ArrayList<Factura> facturas;
-    public Empresa()
-    {
+    private ArrayList<Factura> facturas;
+
+    public Empresa() {
         facturas = new ArrayList<>();
     }
-    public void agregarFactura(Factura factura)
-    {
+
+    public void agregarFactura(Factura factura) {
         facturas.add(factura);
-        System.out.println("aqui"+ facturas);
     }
 
-    public void mostrarFacturas()
-    {
-        for (Factura factura : facturas)
-        {
-            //System.out.println("Cliente: " + factura.cliente.getNombre());
-            //System.out.println("Tipo de vehículo: " + factura.vehiculo.getTipo());
-            //System.out.println("Fecha Inicial: " + formatDate(factura.fechaInicial));
-            // System.out.println("Fecha Final: " + formatDate(factura.fechaFinal));
-            //System.out.println("Costo de alquiler: $" + factura.calcularCostoAlquiler());
-            //System.out.println("------------------------------");
+    public void mostrarFacturas() {
+        for (Factura factura : facturas) {
+            System.out.println("Cliente: " + factura.cliente.getNombre());
+            System.out.println("CC: " + factura.cliente.getId());
+            System.out.println("Telefono: " + factura.cliente.getTelefono());
+            System.out.println("Tipo de vehículo: " + factura.vehiculo.getTipo());
+            System.out.println("Fecha Inicial: " + formatDate(factura.fechaInicial));
+            System.out.println("Fecha Final: " + formatDate(factura.fechaFinal));
+            System.out.println("Costo de alquiler: $" + factura.calcularCostoAlquiler());
+            System.out.println("------------------------------");
         }
     }
 
-    private String formatDate(Date date)
-    {
+    private String formatDate(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         return sdf.format(date);
     }
