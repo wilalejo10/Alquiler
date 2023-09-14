f0ab04d4975d014051cb258c5e26d5aa9044db08
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-13 21:15:29 -0500

    Update Vehiculo.java

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,39 +1,24 @@
  public class Vehiculo
  {
-        private String placa;
-        private String tipo;
-        private String estado;
-        private double capacidad;
-        private double costoPorDia;
+         private String tipo;
+         private double capacidad;
+         private double costoPorDia;
 
-        public Vehiculo(String placa, String tipo,String estado, double capacidad, double costoPorDia)
-        {
-            this.placa = placa;
-            this.tipo = tipo;
-            this.estado = estado;
-            this.capacidad = capacidad;
-            this.costoPorDia = costoPorDia;
-        }
+         public Vehiculo(String tipo, double capacidad, double costoPorDia) {
+             this.tipo = tipo;
+             this.capacidad = capacidad;
+             this.costoPorDia = costoPorDia;
+         }
 
-        public Vehiculo(String tipoVehiculo, double capacidad, double costoPorDia) { }
-        public String getPlaca()
-        {
-            return placa;
-        }
-        public String getTipo()
-        {
-            return tipo;
-        }
-        public String getEstado()
-        {
-            return estado;
-        }
-        public double getCapacidad()
-        {
-            return capacidad;
-        }
-        public double getCostoPorDia()
-        {
-            return costoPorDia;
-        }
- }
+         public String getTipo() {
+             return tipo;
+         }
+
+         public double getCapacidad() {
+             return capacidad;
+         }
+
+         public double getCostoPorDia() {
+             return costoPorDia;
+         }
+     }
